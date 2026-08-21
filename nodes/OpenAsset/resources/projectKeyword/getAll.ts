fa354d4dcba98fee277e6684b80981a5d4664761
Erik Alvarez <erik@tyldum.no>
2026-08-21 12:28:39 +0200

    implemented filter on projectKeyword

--- nodes/OpenAsset/resources/projectKeyword/getAll.ts
+++ nodes/OpenAsset/resources/projectKeyword/getAll.ts
@@ -9,6 +9,26 @@ const showOnlyForProjectKeywordList = {
 };
 
 export const projectKeywordListDescription: INodeProperties[] = [
+	{
+		displayName: 'Query Parameters',
+		name: 'queryParameters',
+		type: 'collection',
+		displayOptions: {
+			show: showOnlyForProjectKeywordList,
+		},
+		default: {},
+		placeholder: 'Add Query Parameter',
+		options: [
+			{
+				displayName: 'Project Keyword Category',
+				name: 'project_keyword_category_id',
+				type: 'string',
+				default: '',
+				description: 'Filter project keywords by linked project keyword category ID',
+			},
+		],
+		description: 'Optional query string filters for the Project Keywords endpoint',
+	},
 	displayFieldsProperty({
 		show: showOnlyForProjectKeywordList,
 	}),
@@ -19,17 +39,19 @@ export async function getProjectKeywords(
 	itemIndex: number,
 ): Promise<IDataObject | IDataObject[]> {
 	const displayFields = this.getNodeParameter('displayFields', itemIndex, '');
+	const queryParameters = withDisplayFieldsQueryParameters(
+		{
+			limit: 0,
+			...((this.getNodeParameter('queryParameters', itemIndex, {}) as IDataObject) ?? {}),
+		},
+		displayFields,
+	);
 
 	return (await openAssetApiRequest.call(
 		this,
 		'GET',
 		'/ProjectKeywords',
 		undefined,
-		withDisplayFieldsQueryParameters(
-			{
-				limit: 0,
-			},
-			displayFields,
-		),
+		queryParameters,
 	)) as IDataObject | IDataObject[];
 }
